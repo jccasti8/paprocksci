1b5d149044c993eb42b1c02e5342d0e5db0f3914
juan david <172879867+jccasti8@users.noreply.github.com>
2025-02-15 23:59:25 -0600

    Write logic to play one round of paper rock scissors
    playRound() fucntion is running and plays a round of paper, rock, scissors, and increments the score depending on who won, computer or human.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ function getRandomInt(max){
 }
 
 function getComputerChoice() {
-    compChoice = getRandomInt(3);
+    let compChoice = getRandomInt(3);
     if (compChoice === 0){
         console.log(compChoice);
         return "paper";
@@ -19,15 +19,61 @@ function getComputerChoice() {
 
 
 }
-console.log(getComputerChoice());
+//console.log("computer choice: " + getComputerChoice());
 
 function getHumanChoice() {
-    humanResponse = window.prompt("Choose Wisely: Paper, Rock, Scissors");
+    let humanResponse = window.prompt("Choose Wisely: Paper, Rock, Scissors");
     return humanResponse;
 }
 
-console.log(getHumanChoice());
+//console.log("human choice: " + getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();  //changes both human choice and computer choice to all caps to adequately compare them.
+    computerChoice = computerChoice.toUpperCase();
+    if (humanChoice==="PAPER"){
+        if (computerChoice === "ROCK"){
+            console.log("You win! 😄 Paper beats rock");
+            humanScore++;
+        }else if(computerChoice === "SCISSORS"){
+            console.log("You lose 😞 Scissors beats paper")
+            computerScore++;
+        }else {
+            console.log("Tied!")
+        }
+    } else if (humanChoice === "ROCK"){
+        if (computerChoice == "PAPER"){
+            console.log("You lose 😞 Paper beats Rock");
+            computerScore++;
+        }else if (computerChoice === "SCISSORS"){
+            console.log("You win! 😄 Rock beats scissors");
+            humanScore++;
+        }else {
+            console.log("Tied!")
+        }
+    } else if (humanChoice === "SCISSORS"){
+        if (computerChoice === "PAPER"){
+            console.log("You win! 😄 Scissors beats paper");
+            humanScore++;
+        } else if (computerChoice === "ROCK"){
+            console.log("You lose 😞 Rock beats scissors");
+            computerScore++;
+        }else {
+            console.log("Tied!")
+        }
+    }
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    
+}
+
+const humanSelection = getHumanChoice();
+console.log("human choice: " + humanSelection);
+const computerSelection = getComputerChoice()
+console.log("computer choice: " + computerSelection);
+
+playRound(humanSelection, computerSelection);
 
